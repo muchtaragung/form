49e7c07a32cdd6db68dae0e1d904b1f027527796
muchtaragung <muchtaragung7@gmail.com>
2021-10-11 15:54:31 +0700

    update footer

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -81,9 +81,7 @@
         </div>
         <!-- /.content-wrapper -->
         <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.1.0
-            </div>
+
             <strong>Copyright &copy; 2021 Korpora Consulting.</strong> All rights reserved.
         </footer>
 
--- application/views/admin/form/list.php
+++ application/views/admin/form/list.php
@@ -89,9 +89,6 @@
         </div>
         <!-- /.content-wrapper -->
         <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.1.0
-            </div>
             <strong>Copyright &copy; 2021 Korpora Consulting.</strong> All rights reserved.
         </footer>
 
